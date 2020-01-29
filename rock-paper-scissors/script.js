8f3b0f39303e8ff8b7ddbcec4e35149d05f57b1f
Alexandra-U <58255196+Alexandra-U@users.noreply.github.com>
2020-01-29 15:25:12 +0200

    commit

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -11,29 +11,32 @@ function playGame() {
     if (userChoice === computerChoice) {
         console.log("Tie!");
     } else if (userChoice === "paper" && computerChoice === "rock") {
-        console.log("Computer chose: Rock");
-        console.log("User chose: Paper");
-        console.log("User wins!");
+        console.log("Computer chose: Rock \n User chose: Paper \n User wins!");
     } else if (userChoice === "rock" && computerChoice === "scissors") {
-        console.log("Computer chose: Scissors");
-        console.log("User chose: Rock");
-        console.log("User wins!");
+        // console.log("Computer chose: Scissors");
+        // console.log("User chose: Rock");
+        // console.log("User wins!");
+        console.log("Computer chose: Scissors \n User chose: Rock \n User wins!");
     } else if (userChoice === "scissors" && computerChoice === "rock") {
-        console.log("Computer chose: Rock");
-        console.log("User chose: Scissors");
-        console.log("User loses!");
+        // console.log("Computer chose: Rock");
+        // console.log("User chose: Scissors");
+        // console.log("User loses!");
+        console.log("Computer chose: Rock \n User chose: Scissors \n User loses!");
     } else if (userChoice === "rock" && computerChoice === "paper") {
-        console.log("Computer chose: Paper");
-        console.log("User chose: Rock");
-        console.log("User loses!");
+        // console.log("Computer chose: Paper");
+        // console.log("User chose: Rock");
+        // console.log("User loses!");
+        console.log("Computer chose: Paper \n User chose: Rock \n User loses!");
     } else if (userChoice === "paper" && computerChoice === "scissors") {
-        console.log("Computer chose: Scissors");
-        console.log("User chose: Paper");
-        console.log("User loses!");
+        // console.log("Computer chose: Scissors");
+        // console.log("User chose: Paper");
+        // console.log("User loses!");
+        console.log("Computer chose: Scissors \n User chose: Paper \n User loses!");
     } else if (userChoice === "scissors" && computerChoice === "paper") {
-        console.log("Computer chose: Paper");
-        console.log("User chose: Scissors");
-        console.log("User wins!");
+        // console.log("Computer chose: Paper");
+        // console.log("User chose: Scissors");
+        // console.log("User wins!");
+        console.log("Computer chose: Paper \n User chose: Scissors \n User wins!");
     }
 }
 
